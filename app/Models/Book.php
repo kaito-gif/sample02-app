ea6b8687f2452224ab8e59f738ec879529724b68
kaito <horumon@gmail.com>
2025-11-03 11:23:15 +0900

    リレーション

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     use HasFactory;
 
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 書籍1件に、複数の著者が紐づくことを定義する
     public function authors(): BelongsToMany
     {
-        return $this->belongsToMany(Author::class)->withTimestamps();
+        return $this->belongsToMany(Author::class, 'author_book', 'book_id', 'author_id')->withTimestamps();
     }
 }
