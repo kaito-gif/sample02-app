a356b2ba10c0f3b33e87d241d5070b2fe28ad1a0
kaito <horumon@gmail.com>
2025-11-05 22:21:21 +0900

    ルーティングとコントローラー

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\Admin\BookController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,10 @@ Route::get('/', function () {
 
 Route::get('messages', [MessageController::class, 'index']);
 Route::post('messages', [MessageController::class, 'store']);
+Route::prefix('admin/books')
+    ->name('book.')
+    ->controller(BookController::class)
+    ->group(function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{id}', 'show')->whereNumber('id')->name('show');
+    });
