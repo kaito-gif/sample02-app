612fd91e7b4e2eb6648d67e961d4f623f9ae9831
kaito <horumon@gmail.com>
2025-11-08 19:31:50 +0900

    リクエスト

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
 use App\Models\Book;
+use App\Models\Category;
 
 class BookController extends Controller
 {
@@ -24,6 +26,34 @@ class BookController extends Controller
         $book = Book::findOrFail($id);
 
         // 取得した書籍をレスポンスとして返す
+        return $book;
+    }
+
+    public function create(): View
+    {
+        // ビューにカテゴリー一覧を表示するために全件取得
+        $categories = Category::all();
+
+        // ビューオブジェクトを返す
+        return view('admin.book.create',[
+            'categories' => $categories
+        ]);
+    }
+
+    public function store(Request $request): Book
+    {
+        // 書籍データ登録用のオブジェクトを作成する
+        $book = new Book();
+
+        // リクエストオブジェクトからパラメータを取得
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        // 保存
+        $book->save();
+
+        // 保存した書籍情報をレスポンスとして返す
         return $book;
     }
 }
