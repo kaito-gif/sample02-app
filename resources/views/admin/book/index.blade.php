31faf99d83f07af54d0cdeaa135b6beaae2b84b0
kaito <horumon@gmail.com>
2025-11-15 01:29:51 +0900

    コンポーネント

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -1,33 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <main>
-        <h1>書籍一覧</h1>
-        @if (session('message'))
-            <div style="color:blue">
-                {{ session('message') }}
-            </div>
-        @endif
-        <a href="{{ route('book.create') }}">追加</a>
-        <table border="1">
-            <tr>
-                <th>カテゴリ</th>
-                <th>書籍名</th>
-                <th>価格</th>
-            </tr>
-            @foreach ($books as $book)
-                <tr @if ($loop->even) style="background:#EEE" @endif>
-                    <td>{{ $book->category->title }}</td>
-                    <td>{{ $book->title }}</td>
-                    <td>{{ $book->price }}</td>
-                </tr>
-            @endforeach
-        </table>
-    </main>
-</body>
-</html>
+<x-layouts.book-manager>
+    <x-slot:title>
+        書籍一覧
+    </x-slot:title>
+    <h1>書籍一覧</h1>
+    @if (session('message'))
+        <x-alert class="info">
+            {{ session('message') }}
+        </x-alert>
+    @endif
+    <a href="{{ route('book.create') }}">追加</a>
+    <x-book-table :$books />
+</x-layouts.book-manager>
