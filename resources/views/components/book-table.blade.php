73c749e3b728710fbf398b208a58fee369156c03
kaito <horumon@gmail.com>
2025-11-18 00:09:19 +0900

    書籍更新機能の作成

--- resources/views/components/book-table.blade.php
+++ resources/views/components/book-table.blade.php
@@ -4,6 +4,7 @@
             <th>カテゴリ</th>
             <th>書籍名</th>
             <th>価格</th>
+            <th>更新</th>
         </tr>
         @foreach ($books as $book)
             <tr @if ($loop->even) style="background:#EEE" @endif>
@@ -14,6 +15,11 @@
                     </a>
                 </td>
                 <td>{{ $book->price }}</td>
+                <td>
+                    <a href="{{ route('book.edit', $book) }}">
+                        <button>更新</button>
+                    </a>
+                </td>
             </tr>
         @endforeach
     </table>
