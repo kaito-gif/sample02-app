fe78a80637d6ae66c5c88eb164ceb74844a43506
kaito <horumon@gmail.com>
2025-11-19 00:30:02 +0900

    Laravel Breeze

---
+++ routes/_web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\Admin\BookController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('messages', [MessageController::class, 'index']);
+Route::post('messages', [MessageController::class, 'store']);
+Route::prefix('admin/books')
+    ->name('book.')
+    ->controller(BookController::class)
+    ->group(function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{book}', 'show')->whereNumber('book')->name('show');
+        Route::get('create', 'create')->name('create');
+        Route::post('', 'store')->name('store');
+        Route::get('{book}/edit', 'edit')->whereNumber('book')->name('edit');
+        Route::put('{book}', 'update')->whereNumber('book')->name('update');
+        Route::delete('{book}', 'destroy')->whereNumber('book')->name('destroy');
+    });
